96d6c2394bc999583220d3f91ae168a9e81b60ea
oksirets <kukharets1bs.17b@gmail.com>
2020-09-12 17:43:58 +0300

    Refactoring

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
 import classes from './Dialogs.module.css'
-import { NavLink } from 'react-router-dom';
-
-const DialogItem = (props) => {
-    return (
-        <div className = {classes.dialog + ' ' + classes.active}>
-            <NavLink to = {"/Dialogs/" + props.id}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <div className = {classes.message}>
-            {props.message}
-        </div>
-    )
-}
+import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message';
 
 const Dialogs = (props) => {
     let dialogs = [
